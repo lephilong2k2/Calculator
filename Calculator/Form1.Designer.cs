162747392beb32f1a6635739bc42381702290875
lephilong2k2 <lephilong1907@gmail.com>
2022-10-16 14:12:32 +0700

    Them giao dien nut cong

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -28,12 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtKetQua = new System.Windows.Forms.TextBox();
+            this.lbA = new System.Windows.Forms.Label();
+            this.lbB = new System.Windows.Forms.Label();
+            this.lbKQ = new System.Windows.Forms.Label();
+            this.btCong = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(286, 123);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(346, 38);
+            this.txtA.TabIndex = 0;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(286, 205);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(346, 38);
+            this.txtB.TabIndex = 0;
+            // 
+            // txtKetQua
+            // 
+            this.txtKetQua.Location = new System.Drawing.Point(286, 275);
+            this.txtKetQua.Name = "txtKetQua";
+            this.txtKetQua.Size = new System.Drawing.Size(346, 38);
+            this.txtKetQua.TabIndex = 0;
+            // 
+            // lbA
+            // 
+            this.lbA.AutoSize = true;
+            this.lbA.Location = new System.Drawing.Point(174, 133);
+            this.lbA.Name = "lbA";
+            this.lbA.Size = new System.Drawing.Size(80, 31);
+            this.lbA.TabIndex = 1;
+            this.lbA.Text = "Số A:";
+            // 
+            // lbB
+            // 
+            this.lbB.AutoSize = true;
+            this.lbB.Location = new System.Drawing.Point(174, 212);
+            this.lbB.Name = "lbB";
+            this.lbB.Size = new System.Drawing.Size(80, 31);
+            this.lbB.TabIndex = 1;
+            this.lbB.Text = "Số B:";
+            // 
+            // lbKQ
+            // 
+            this.lbKQ.AutoSize = true;
+            this.lbKQ.Location = new System.Drawing.Point(151, 278);
+            this.lbKQ.Name = "lbKQ";
+            this.lbKQ.Size = new System.Drawing.Size(115, 31);
+            this.lbKQ.TabIndex = 1;
+            this.lbKQ.Text = "Kết quả:";
+            // 
+            // btCong
+            // 
+            this.btCong.Location = new System.Drawing.Point(396, 417);
+            this.btCong.Name = "btCong";
+            this.btCong.Size = new System.Drawing.Size(137, 42);
+            this.btCong.TabIndex = 2;
+            this.btCong.Text = "Cộng";
+            this.btCong.UseVisualStyleBackColor = true;
+            this.btCong.Click += new System.EventHandler(this.btCong_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1045, 878);
+            this.Controls.Add(this.btCong);
+            this.Controls.Add(this.lbKQ);
+            this.Controls.Add(this.lbB);
+            this.Controls.Add(this.lbA);
+            this.Controls.Add(this.txtKetQua);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtKetQua;
+        private System.Windows.Forms.Label lbA;
+        private System.Windows.Forms.Label lbB;
+        private System.Windows.Forms.Label lbKQ;
+        private System.Windows.Forms.Button btCong;
     }
 }
 
